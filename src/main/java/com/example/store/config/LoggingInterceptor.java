fbcc06f70ae9e67cca9c04e0f92203c6f6018f22
Ankit Agarwal - Consultant <ankit.agarwal@wadhwanifoundation.org>
2024-12-24 21:36:08 +0530

    Interceptor for logging response time

---
+++ src/main/java/com/example/store/config/LoggingInterceptor.java
@@ -0,0 +1,32 @@
+package com.example.store.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.setAttribute("startTime", System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        long startTime = (long) request.getAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        logger.info("[{}] {} - {} ms", request.getMethod(), request.getRequestURI(), executionTime);
+    }
+}
